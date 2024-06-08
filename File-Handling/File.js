21f67bd5d52e86b4725c0b38cd44782e5676e677
Anirban Mondal <developerani45@gmail.com>
2024-06-08 21:11:42 +0530

    Node Js Architecture done

--- File-Handling/File.js
+++ File-Handling/File.js
@@ -15,7 +15,7 @@ const fs  = require('fs') //fs is the package for file
 
 console.log(result) */
 
-
+//non - blocking code
 /* fs.readFile('./Contact.txt', "utf-8", (err, result) => {
     if(err) {
         console.log(`error ${err}`);
@@ -26,6 +26,17 @@ console.log(result) */
 
 
 
-fs.appendFileSync('./Contact.txt', `${Date.now()} \n`)
+/* fs.appendFileSync('./Contact.txt', `${Date.now()} \n`) */ // blocking code
 
 // fs.mkdirSync('my-file/a/b', {recursive: true})
+
+
+const os = require('os')
+console.log(os.cpus().length)
+
+
+/* generally the thread pool size is = 4
+but we can extend thread pool size, that is depends on the size of cpu core.
+i.e my cpu core size = 16
+that mean I can expand thread pool size upto 16
+*/
