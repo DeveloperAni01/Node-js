ce8fd682dd0e09887e900b64603e2e9179721283
Anirban Mondal <developerani45@gmail.com>
2024-06-09 11:10:36 +0530

    Learn how Server handles Queries

--- HTTP-Server/index.js
+++ HTTP-Server/index.js
@@ -1,18 +1,23 @@
 const http = require ('http')
 const fs = require('fs')
+const url = require('url')
 
 
 const myServer = http.createServer ((req, res) => {
     // console.log('received req from client')
 
     const log = `${Date.now()}: ${req.url} New Request Received \n`
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
+    console.log(myUrl.pathname);
     fs.appendFile('log.txt',log, (err, data) => {
-        switch(req.url) {
+        switch(/* req.url */ myUrl.pathname) {
             case '/' : 
                 res.end(`welcome to server !`);
                 break;
             case '/about': 
-                res.end(`hey I am Anirban Mondal`);
+                const username = myUrl.query.username
+                res.end(`hey I am ${username}`);
                 break;
             default:
                 res.end(`404 not found!`);
