bf61df89a748a6d8fce6d84011b87c770ecb3ed0
Anirban Mondal <developerani45@gmail.com>
2024-06-14 17:40:58 +0530

    updated

--- Learning-MongoDB/MiddleWares/index.js
+++ Learning-MongoDB/MiddleWares/index.js
@@ -1,1 +1,14 @@
 const fs = require('fs')
+
+function logReqRes(filename) {
+    return(req, res, next) => {
+        fs.appendFile('file name', data, (err, data) => {
+            next();
+        })
+    }
+}
+
+
+module.exports = {
+    logReqRes
+}
--- Learning-MongoDB/index.js
+++ Learning-MongoDB/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const {connectMongoDb} = require('./Connection.js')
+
+
+const {logReqRes} = ('./MiddleWares')
+
 const userRouter = require('./Routes/Routes.js')
 
-
-const {connectMongoDb} = require('./Connection.js')
 
 const app = express()
 const PORT = 8000;
@@ -12,12 +15,9 @@ const PORT = 8000;
 connectMongoDb("'mongodb://127.0.0.1:27017/testing-database-2'")
     
 
-
-
-
-
 // //Middleware -- plagin
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({extended: false})); 
+app.use(logReqRes("log.txt"))
 
 
 //Routers
